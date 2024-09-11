545fbce13f71c028a85a04c5a955c2207772ab3a
Kasht <kubtimur1@mail.com>
2024-09-11 22:00:07 +0500

    init git repository for project named green shop

---
+++ src/main/java/kz/greenshop/greenshop/Controllers/HomeController.java
@@ -0,0 +1,15 @@
+package kz.greenshop.greenshop.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String getHomePage() {
+
+        return "home";
+    }
+
+}
